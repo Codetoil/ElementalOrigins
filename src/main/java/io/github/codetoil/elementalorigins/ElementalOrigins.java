a204e68d93eebd4cab985caaf808d8ac09e2487d
Anthony (Codetoil) <ianthisawesomee@gmail.com>
2023-02-09 20:57:01 +0000

    Stuff

--- src/main/java/io/github/codetoil/elementalorigins/ElementalOrigins.java
+++ src/main/java/io/github/codetoil/elementalorigins/ElementalOrigins.java
@@ -16,6 +16,6 @@ public class ElementalOrigins implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("Initializing Elemental Origins");
 	}
 }
